aa5ec17cf71c6fce45cbf65c247b7ccb62c6e035
惜朋 <shafeng.sf@alibaba-inc.com>
2015-05-05 18:16:08 +0800

    by shafeng

--- _posts/core-samples/2015-5-5-performance-think.php
+++ _posts/core-samples/2015-5-5-performance-think.php
@@ -8,11 +8,14 @@ tagline: ""
 
 最近比较喜欢从一个点想开来的方式去组织前端的知识点，比如从“性能优化”，“模块化”，“CSS组件”等等这样的点，联想开来。
 
+
 就拿“模块化”这个点来说，会联想到：什么是模块化、模块化的优缺点、如何组织模块、如何拆分和组合模块、如何编写模块、如何引用模块等等，想来开了之后，前端的知识点很多就都可以串联起来了。
 
 
 下面是我从“性能优化”角度能想到的关于它的一些方面内容：
 
-![](http://bcs.duapp.com/diandiblog/%E6%80%A7%E8%83%BD%E4%BC%98%E5%8C%96.png)
 
-附加一篇检测页面加载时间的文章：[页面加载时间度量](http://oldj.net/article/measuring-the-user-latency/)
+![image](http://bcs.duapp.com/diandiblog/%E6%80%A7%E8%83%BD%E4%BC%98%E5%8C%96.png)
+
+
+附加一篇检测页面加载时间的文章： [页面加载时间度量](http://oldj.net/article/measuring-the-user-latency/)
